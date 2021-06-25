d0296cb93fa1c33ffd2bf4385030c47bcde0e1fd
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-26 00:09:25 +0530

    Update and rename Strings.cpp to String-Functions.cpp

---
+++ C++/String-Functions.cpp
@@ -0,0 +1,26 @@
+#getline
+#push-back
+#pop-back
+#getline
+
+
+#include<iostream>
+#include<string> 
+using namespace std;
+int main()
+{
+    string str,str2,str3;
+  
+    getline(cin,str);                               // Taking string input using getline()
+    cout<<"The initial string is -> ";
+    cout<<str<<endl;
+   
+    str.push_back('s');                                  // Using push_back() to insert a character at end
+    cout<<"The string after push_back operation is -> ";
+    cout<<str<<endl;
+ 
+    str.pop_back();                                 // Using pop_back() to delete a character from end
+    cout<<"The string after pop_back operation is -> ";
+    cout<<str<<endl;
+  
+    cout<<"The length of the string is :"<<str.length()<<endl;
