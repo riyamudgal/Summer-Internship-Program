4814f174f1794b91c64437519a6a542cac080863
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-30 21:07:15 +0530

    Update Tower of Hanoi.cpp

--- DSA/Day 1 Arrays/Day 1 Array/Recursion/Tower of Hanoi.cpp
+++ DSA/Day 1 Arrays/Day 1 Array/Recursion/Tower of Hanoi.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void tower(int n, char source, char destination, char middle) 
+{ 
+    
+    if (n == 1) 
+    { 
+        cout<<endl<<"Move disk 1 from rod"<<source<<" to rod "<<destination<<endl; 
+        return; 
+    } 
+    
+    tower(n - 1, source, middle, destination); 
+    cout<<"Move disk "<<n<<" from rod "<<source<<" to rod "<<destination<<endl; 
+    tower(n - 1, middle, destination, source); 
+} 
+int main() 
+{ 
+    int n;
+    cout<<"Enter the number of disks you have in a rod-> ";
+    cin>>n;
+    cout<<endl<<"Here are the steps to be taken ->";
+    tower(n, 'A', 'C', 'B');  
+    return 0; 
+} 
 
