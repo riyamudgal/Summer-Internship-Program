571f39cca39c7d764d36912b95ece4457d28a2a0
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-02 21:19:11 +0530

    Create Merged Sorted Array.cpp

---
+++ LeetCode-Programs/Merged Sorted Array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        vector<int> vec;
+        while (i<m && j<n)
+        {
+            if (nums1[i]<nums2[j])
+            {
+                vec.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                vec.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i<m)
+        {
+            vec.push_back(nums1[i]);
+            i++;
+        }
+        while (j<n)
+        {
+            vec.push_back(nums2[j]);
+            j++;
+        }
+
+        for (i=0;i<vec.size();i++)
+        {
+            nums1[i]=vec[i];
+        }
+    }
+};
