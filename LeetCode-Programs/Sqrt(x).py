ce9af563fcd48e0a2246d4a5b849024efb9130a3
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-07 22:09:01 +0530

    Create Sqrt(x).py

---
+++ LeetCode-Programs/Sqrt(x).py
@@ -0,0 +1,7 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        s=math.sqrt(x)
+        return trunc(s)
+      
+      
+      #trunc() method returns the truncated integer part of a number.
