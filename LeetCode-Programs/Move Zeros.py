1550f63e74fa1c4f9578bd70486a2b0a6297d24f
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-12 00:12:11 +0530

    Update Move Zeros.py

--- LeetCode-Programs/Move Zeros.py
+++ LeetCode-Programs/Move Zeros.py
@@ -8,4 +8,8 @@ class Solution:
             if i==0:
                 nums.remove(i)
                 nums.append(i)
+                
+                
+                
+#Here at first Zero id removed from the list then it is appended at the end of list
       
