947e0bbdcc903e4769ed15907e7ba96ee3422ae7
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-17 15:38:05 +0530

    Update Single Number.cpp

--- LeetCode-Programs/Single Number.cpp
+++ LeetCode-Programs/Single Number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> c;
         for(int i=0;i<nums.size();i++)
         {
             if(count(nums.begin(),nums.end(),nums[i])==1)
