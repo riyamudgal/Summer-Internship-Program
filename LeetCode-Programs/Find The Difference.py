140eac61fe5aa19bfec29e0e69a80b93dab227f7
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-18 12:59:35 +0530

    Update Find The Difference.py

--- LeetCode-Programs/Find The Difference.py
+++ LeetCode-Programs/Find The Difference.py
@@ -3,3 +3,21 @@ class Solution:
         for i in set(t):
             if s.count(i)!=t.count(i):
                 return i
+            
+            
+            
+#2nd method
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        zs=sorted(s)
+        zt=sorted(t)
+        i=0
+        j=0
+        while i<len(zs):
+            if zs[i]!=zt[j]:
+                return zt[j]
+            i=i+1
+            j=j+1
+        
+        return zt[len(zt)-1]
