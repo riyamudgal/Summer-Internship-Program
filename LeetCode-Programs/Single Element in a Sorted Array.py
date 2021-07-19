f2d97739f354acb75468d5935cb0639c668d77ec
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-20 01:24:53 +0530

    Update Single Element in a Sorted Array.py

--- LeetCode-Programs/Single Element in a Sorted Array.py
+++ LeetCode-Programs/Single Element in a Sorted Array.py
@@ -4,3 +4,22 @@ class Solution:
         for i in count:
             if count[i]==1:
                 return i
+            
+#using binary search
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l=0
+        h=len(nums)-1
+        while l<h:
+            mid=(l+h)//2
+            if mid%2==1:
+                if nums[mid-1]==nums[mid]:
+                    l=mid+1
+                else:
+                    h=mid
+            else:
+                if nums[mid]!= nums[mid+1]:
+                    h=mid
+                else:
+                    l=mid+1
+        return nums[l]
