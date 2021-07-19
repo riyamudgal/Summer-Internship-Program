f62becf4bbfe79d4c6807c074f34a374d0e75d9a
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-20 00:08:07 +0530

    Update Single Element in a Sorted Array.cpp

--- LeetCode-Programs/Single Element in a Sorted Array.cpp
+++ LeetCode-Programs/Single Element in a Sorted Array.cpp
@@ -9,3 +9,41 @@ public:
         return 0;
     }
 };
+
+
+#using binary search
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        while(l<h) 
+        {
+        int mid=(l+h)/2;
+        if (mid%2==1) 
+        {
+                if (nums[mid-1]==nums[mid]) 
+                {
+                    l=mid+1;
+                } 
+            else 
+            {
+                    h=mid;
+            }
+        } 
+            else 
+            {
+                if (nums[mid]!=nums[mid+1]) 
+                {
+                    h=mid;
+                }
+                else 
+                {
+                    l=mid+1;
+                }
+            }
+        } 
+        return nums[l];
+    }
+};
