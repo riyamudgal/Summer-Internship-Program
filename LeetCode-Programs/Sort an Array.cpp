769b8a9bdf5779e2ac6ada5d0a17699eb8d06e54
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-20 17:00:25 +0530

    Create Sort an Array.cpp

---
+++ LeetCode-Programs/Sort an Array.cpp
@@ -0,0 +1,11 @@
+#using function
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+#TLE in insertion, bubble, selection sorting,quick sort
