7a96c80a469963005805e051a0ec2d11a419a0d9
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-25 02:00:11 +0530

    Update Transpose Matrix.cpp

--- LeetCode-Programs/Transpose Matrix.cpp
+++ LeetCode-Programs/Transpose Matrix.cpp
@@ -18,3 +18,22 @@ public:
         
     }
 };
+
+
+#second 
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+       int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i=0;i<m;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                mat[j][i]=matrix[i][j];
+            }
+        }
+        return mat;
+    }
+};
