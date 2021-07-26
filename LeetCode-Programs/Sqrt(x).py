b34f556323a003d38efeafbc94e45f275692cf6c
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-26 15:27:18 +0530

    Update Sqrt(x).py

--- LeetCode-Programs/Sqrt(x).py
+++ LeetCode-Programs/Sqrt(x).py
@@ -5,3 +5,23 @@ class Solution:
       
       
       #trunc() method returns the truncated integer part of a number.
+        
+        
+#without using inbuilt function
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        start=0
+        end=x
+        
+        while start<=end:
+            mid=(start+end)//2
+            
+            if mid*mid>x:
+                end=mid-1
+            elif mid*mid<x:
+                start=mid+1
+            else:
+                return mid
+        
+        
+        return end
