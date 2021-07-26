589bffd90bd96309a1395b410d22ab4a46c1f436
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-26 15:33:03 +0530

    Update Sqrt(x).cpp

--- LeetCode-Programs/Sqrt(x).cpp
+++ LeetCode-Programs/Sqrt(x).cpp
@@ -5,3 +5,27 @@ public:
         return s;
     }
 };
+
+#without using built in function
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long start=0,end=x,mid;
+        if (x<2)
+            return x;
+        while (start<=end)
+        {
+            mid=(start+end)/2;
+            
+            if (mid*mid>x)
+                end=mid-1;
+            else if (mid*mid<x)
+                start=mid+1;
+            else
+                return mid;
+        
+        }
+        return end;
+        
+    }
+};
