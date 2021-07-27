0154bb9fd5ca979cc5e53d8c158d6a326eaf1f12
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-27 22:10:06 +0530

    Update XOR Operation in an Array.cpp

--- LeetCode-Programs/XOR Operation in an Array.cpp
+++ LeetCode-Programs/XOR Operation in an Array.cpp
@@ -14,3 +14,17 @@ public:
         return ans;
     }
 };
+
+
+#second
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=ans^(start+2*i);
+        }
+        return ans;
+    }
+};
