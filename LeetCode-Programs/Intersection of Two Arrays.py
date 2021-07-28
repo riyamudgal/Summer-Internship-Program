c5ac6f4b96c87db0e876fbfde4fcaa865c81cc59
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-28 22:35:47 +0530

    Update Intersection of Two Arrays.py

--- LeetCode-Programs/Intersection of Two Arrays.py
+++ LeetCode-Programs/Intersection of Two Arrays.py
@@ -3,3 +3,34 @@ class Solution:
         nums3=[value for value in nums1 if value in nums2]
         nums3= list(set(nums3))
         return nums3
+    
+    
+    
+    
+    
+    #second
+    nums1.sort()
+    nums2.sort()
+    n=len(nums1)
+    m=len(nums2)
+        
+    i=0
+    j=0
+    res=[]
+    while(i<n  and j<m):
+         if(nums1[i]>nums2[j]):
+             j=j+1
+                
+         elif (nums2[j]>nums1[i]):
+              i=i+1
+                
+         else:
+              res.append(nums1[i])
+              i=i+1
+              j=j+1
+                
+         res= list(set(res))
+        return res
+                    
+    
+    
