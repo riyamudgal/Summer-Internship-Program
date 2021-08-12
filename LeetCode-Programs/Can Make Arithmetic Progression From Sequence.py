aa4697e69c049872619fb39a35f85cbda902b46c
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-12 20:35:31 +0530

    Create Can Make Arithmetic Progression From Sequence.py

---
+++ LeetCode-Programs/Can Make Arithmetic Progression From Sequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        dif=arr[1]-arr[0];
+        
+        for i in range(len(arr)-1):
+            if arr[i+1]-arr[i]!=dif:
+                return False
+        return True
