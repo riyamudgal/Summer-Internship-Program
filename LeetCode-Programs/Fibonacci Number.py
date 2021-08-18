8b312ee629d6008b3fe80c0ffea9c69149702b71
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-18 23:51:46 +0530

    Update Fibonacci Number.py

--- LeetCode-Programs/Fibonacci Number.py
+++ LeetCode-Programs/Fibonacci Number.py
@@ -5,3 +5,14 @@ class Solution:
         if n==1:
             return 1
         return self.fib(n-1) + self.fib(n-2)               #self, beacuse we are calling the same function recursively
+
+    
+#second way
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        for i in range(n):
+            a,b=b,a+b
+        return a
+    
