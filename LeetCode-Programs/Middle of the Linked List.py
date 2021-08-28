d63337356750aeeb5986b9f43f5b9fdfeea03afe
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-28 20:48:51 +0530

    Create Middle of the Linked List.py

---
+++ LeetCode-Programs/Middle of the Linked List.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        md= [head]
+        while md[-1].next:
+            md.append(md[-1].next)
+            #md=md.next
+        return md[len(md)//2]
+       
