573afe07e126720d7d60c4ff678135e4bdd3ccf3
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-30 00:03:43 +0530

    Update Middle of the Linked List.py

--- LeetCode-Programs/Middle of the Linked List.py
+++ LeetCode-Programs/Middle of the Linked List.py
@@ -10,4 +10,20 @@ class Solution:
             md.append(md[-1].next)
             #md=md.next
         return md[len(md)//2]
+    
+    
+    
+#second
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        self.md=[]
+        while head:
+            self.md.append(head)
+            head=head.next
+        return self.md[len(self.md)//2]
        
