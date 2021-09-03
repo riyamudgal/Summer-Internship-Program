d10443074a4851a46fe6512d3ebcb107a91b3f2f
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-09-03 23:57:02 +0530

    Create First Bad Version.cpp

---
+++ LeetCode-Programs/First Bad Version.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+       int l=1;
+        int r=n;
+        while(l<r)
+        {
+            unsigned int mid=l+(r-l)/2;
+            if(isBadVersion(mid))
+                r=mid;
+            else
+                l=mid+1;
+        }
+        return l;
+    }
+};
