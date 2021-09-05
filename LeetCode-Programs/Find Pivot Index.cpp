2b36ac0861664ae9875ee829e5fa4ad2d1be690c
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-09-05 15:52:59 +0530

    Update Find Pivot Index.cpp

--- LeetCode-Programs/Find Pivot Index.cpp
+++ LeetCode-Programs/Find Pivot Index.cpp
@@ -14,6 +14,22 @@ public:
     }
 };
 
+//second approach
+class Solution {
+public:
+int pivotIndex(vector<int>& nums) {
+   int summ=accumulate(nums.begin(),nums.end(),0);
+        int l_sum=0;
+        for (int i=0;i<nums.size();i++) 
+        {
+            if(l_sum==summ-l_sum-nums[i]) 
+                return i;
+            l_sum=l_sum+nums[i];
+        }
+        return -1;
+    }
+};
+
 /*
 accumulate(first, last, sum);
 first, last : first and last elements of range whose elements are to be added
