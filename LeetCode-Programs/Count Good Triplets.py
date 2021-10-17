3d4d40130c880f5cecfecb7d6830bdba29675e12
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-10-18 00:03:20 +0530

    Create Count Good Triplets.py

---
+++ LeetCode-Programs/Count Good Triplets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        count=0
+        for i in range(0, len(arr)-2):
+            #j=i+1
+            for j in range(i+1, len(arr)-1):
+            
+                if abs(arr[i]-arr[j])<=a:
+                    #k=j+1
+                    for k in range(j+1, len(arr)):
+                    
+                        if abs(arr[j]-arr[k])<=b and abs(arr[i]-arr[k])<=c:
+                            count+=1
+        return count
