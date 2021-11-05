a24754f1450d1db6c1f69af3048833057eea02aa
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-05 13:39:48 +0530

    Create Peak Index in a Mountain Array.cpp

---
+++ LeetCode-Programs/Peak Index in a Mountain Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i;
+        for(i=0;i<arr.size();i++)
+            if(arr[i]>arr[i+1])
+                break;
+            return i;
+    }
+};
