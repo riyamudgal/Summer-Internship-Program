1aaa8bd4b25f583ad97b15c93234710910fe38a8
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-08 00:02:42 +0530

    Update and rename Build Array from Permutation..py to Build Array from Permutation.py

--- LeetCode-Programs/Build Array from Permutation.py
+++ LeetCode-Programs/Build Array from Permutation.py
@@ -4,3 +4,12 @@ class Solution:
         for i in range(len(nums)):
             new_list.append(nums[nums[i]])
         return new_list
+    
+    
+#second
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        new_list=[]
+        for i in nums:
+            new_list.append(nums[i])
+        return new_list
