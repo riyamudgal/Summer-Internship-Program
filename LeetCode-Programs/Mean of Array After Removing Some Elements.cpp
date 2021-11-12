fdbe49acdbb87ccce24c065cec7f7280c7822c4f
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-12 23:52:06 +0530

    Update Mean of Array After Removing Some Elements.cpp

--- LeetCode-Programs/Mean of Array After Removing Some Elements.cpp
+++ LeetCode-Programs/Mean of Array After Removing Some Elements.cpp
@@ -17,3 +17,26 @@ public:
         return (sum/arr.size());
     }
 };
+
+
+
+
+//second
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+        int n=arr.size(),count=0;
+        sort(arr.begin(),arr.end());
+        int per=n*5/100;
+        double sum=0,ans=0;
+        for(int i=per;i<n-per;i++)
+        {
+            count++;
+            sum=sum+arr[i];
+        }
+        ans=sum/count;
+        return ans;
+    }
+};
+    }
+};
