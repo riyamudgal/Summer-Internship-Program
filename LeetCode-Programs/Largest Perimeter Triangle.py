6f0dde89e81ed9c6f5fa890c14096c6a98504ec5
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-15 02:50:05 +0530

    Create Largest Perimeter Triangle.py

---
+++ LeetCode-Programs/Largest Perimeter Triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        '''nums.sort()
+        per=0
+        if nums[0]+nums[1]>nums[2]:
+            return nums[0]+nums[1]+nums[2]
+        return 0
+        '''
+        nums.sort()
+        per=0
+        for i in range(len(nums)-2):
+            if nums[i]+nums[i+1]>nums[i+2]:
+                per=nums[i]+nums[i+1]+nums[i+2]
+        return per
+            
