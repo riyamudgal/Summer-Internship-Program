9e5047b05aee2d28bb7e24df11c662f3c1457172
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-19 18:20:12 +0530

    Update Minimum Number of Moves to Seat Everyone.cpp

--- LeetCode-Programs/Minimum Number of Moves to Seat Everyone.cpp
+++ LeetCode-Programs/Minimum Number of Moves to Seat Everyone.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(students.begin(),students.end());
         sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
         int sum=0;
         for(int i=0;i<seats.size();i++)
         {
