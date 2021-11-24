67771d8d3532d24613e6c30a242fdd5ff0d95a01
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-25 00:30:55 +0530

    Update Length of Last word.cpp

--- LeetCode-Programs/Length of Last word.cpp
+++ LeetCode-Programs/Length of Last word.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
          int length=0;
-        for (int i = s.size() - 1; i >= 0; i--) 
+        for (int i=s.size()-1;i>= 0;i--) 
         {
             if (s[i]!=' ') 
                 length=length+1;
-            if (len>0 && s[i]==' ')
+            if (length>0 && s[i]==' ')
                 break;
         }
         return length;
