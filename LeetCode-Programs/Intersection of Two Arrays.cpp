24238b80245c67cb41c3f8043664965340c9079b
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-11-30 00:08:16 +0530

    Update Intersection of Two Arrays.cpp

--- LeetCode-Programs/Intersection of Two Arrays.cpp
+++ LeetCode-Programs/Intersection of Two Arrays.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums2.begin(), nums2.end());
         int n=nums1.size();
         int m=nums2.size();
-        vector<int> res;
+        vector<int> ans;
         int i=0,j=0;
         while (i<n && j<m) {
         if (nums1[i]>nums2[j]) 
@@ -18,12 +18,12 @@ public:
         }
         else
         {
-            res.push_back(nums1[i]);
+            ans.push_back(nums1[i]);
             i++;
             j++;
         }
     }
-        res.erase(unique(ans.begin(),ans.end()),ans.end());
-        return res;
+        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        return ans;
     }
 };
