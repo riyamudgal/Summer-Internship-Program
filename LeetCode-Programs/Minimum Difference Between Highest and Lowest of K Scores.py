7c343ab1b065b739f81ea00b88c471234f4ec7e2
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-12-07 01:02:18 +0530

    Create Minimum Difference Between Highest and Lowest of K Scores.py

---
+++ LeetCode-Programs/Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        res=nums[k-1]-nums[0]
+        #i=k
+        for i in range(k,len(nums)):
+            res=min(res, nums[i]-nums[i-k+1])
+        return res
