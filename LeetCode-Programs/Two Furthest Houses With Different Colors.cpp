5be84a79ecbe7659036c8bb7aba872d364a275f3
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-12-26 16:44:38 +0530

    Create Two Furthest Houses With Different Colors.cpp

---
+++ LeetCode-Programs/Two Furthest Houses With Different Colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int maxx=0;
+        for(int i=0;i<colors.size();i++)
+        {
+            for(int j=i;j<colors.size();j++)
+            {
+                if(colors[i]!=colors[j])
+                    maxx=max(j-i, maxx);
+            }
+        }
+        return maxx;
+    }
+};
