2a864e5dd4370048715cc4405c83d7ac6a167635
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-01-03 23:58:58 +0530

    Update A Number After a Double Reversal.py

--- LeetCode-Programs/A Number After a Double Reversal.py
+++ LeetCode-Programs/A Number After a Double Reversal.py
@@ -16,3 +16,34 @@ class Solution:
             return True
         else:
             return False
+        
+        
+        
+        
+        
+        
+        
+ class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        '''temp=num
+        rev=0
+        while temp>0:
+            rev=rev*10 + temp%10
+            temp=temp/10
+        temp=rev
+        temp2=0
+        while temp>0:
+            temp2=temp2*10 + temp%10
+            temp=temp/10
+        if num==temp2:
+            return True
+        else:
+            return False'''
+        
+        '''rev=int(str(num(::-1)))
+        reve=int(str(rev(::-1)))
+        if num==reve:
+            return True
+        else:
+            return False'''
+        return num==int(str(int(str(num)[::-1]))[::-1])
