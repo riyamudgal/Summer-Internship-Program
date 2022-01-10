9e60c6ecb479e22166dfce8331a4e02f771bbed1
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-01-10 22:20:16 +0530

    Create Find the Highest Altitude.cpp

---
+++ LeetCode-Programs/Find the Highest Altitude.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int> vec;
+        int alt=0;
+        vec.push_back(0);
+        for(int i=0;i<gain.size();i++)
+        {
+            alt=vec[i]+gain[i];
+            vec.push_back(alt);
+        }
+        int ans=vec[0];
+        for(int i=1;i<vec.size();i++)
+        {
+            if(vec[i]>ans)
+                ans=vec[i];
+        }
+        return ans;
+    }
+};
