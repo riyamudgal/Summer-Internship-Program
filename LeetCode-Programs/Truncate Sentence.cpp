9de1330d744e8c212d6adb6891721cdd1b8b6bd0
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-01-14 17:42:34 +0530

    Create Truncate Sentence.cpp

---
+++ LeetCode-Programs/Truncate Sentence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ')
+                count++;
+            if(count==k)
+                s.erase(s.begin()+i, s.end());
+        }
+        return s;
+    }
+};
