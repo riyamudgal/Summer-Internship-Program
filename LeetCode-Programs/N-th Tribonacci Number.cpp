043e2b69458429a466b7540962a5d2cdf63e678e
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-02-05 23:53:58 +0530

    Create N-th Tribonacci Number.cpp

---
+++ LeetCode-Programs/N-th Tribonacci Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        //tle
+        /*if (n==0)
+            return  0;
+        if (n==1 || n==2)
+            return 1;            
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);*/
+      
+        int arr[n+3];
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=1;
+        for(int i=3;i<=n;i++)
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        return arr[n];*/
+    }
+};
