b4c50901227c87168f72b3dae2fdabb88fd18238
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-03-01 21:05:37 +0530

    Create Counting Bits.cpp

---
+++ LeetCode-Programs/Counting Bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        int count=0;
+        vector<int> vec;
+        for(int i=0;i<n+1;i++)
+        {
+            int temp=i;
+            while(temp!=0)
+            {
+                if(temp%2==1)
+                    count++;
+                temp=temp/2;
+            }
+            vec.push_back(count);
+            count=0;
+        }
+        return vec;
+    }
+};
