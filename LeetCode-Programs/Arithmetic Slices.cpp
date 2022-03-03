e2250e74363d2658c4d09d1ef066c0ef1a2e2852
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-03-03 16:22:44 +0530

    Update Arithmetic Slices.cpp

--- LeetCode-Programs/Arithmetic Slices.cpp
+++ LeetCode-Programs/Arithmetic Slices.cpp
@@ -16,3 +16,27 @@ public:
         return count;
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int count=0, ans=0;
+        for(int i=1; i<nums.size()-1; i++)
+        {
+            if(nums[i]-nums[i-1]==nums[i+1]-nums[i])
+            {
+                count++;
+                ans=ans+count;
+            }
+            else
+                count=0;
+        }
+        return ans;
+    }
+};
