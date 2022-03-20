db1e002d662c1ed632e23fe2d3d098e86bb8a512
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-03-20 17:40:52 +0530

    Update Is Subsequence.cpp

--- LeetCode-Programs/Is Subsequence.cpp
+++ LeetCode-Programs/Is Subsequence.cpp
@@ -12,3 +12,24 @@ public:
         return false;
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0, j=0;
+        while( i<s.length() && j<t.length()) {
+            if(s[i]==t[j])
+                i+=1;
+            j+=1;
+        }
+        if(i==s.length())
+            return true;
+        return false;
+    }
+};
