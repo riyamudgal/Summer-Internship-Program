962ba25133b60a8f0c5cda275fc1a0877e022bae
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-04-01 00:01:21 +0530

    Update Shuffle String.cpp

--- LeetCode-Programs/Shuffle String.cpp
+++ LeetCode-Programs/Shuffle String.cpp
@@ -15,3 +15,20 @@ public:
         return str;
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string str=s;
+        for(int i=0;i<indices.size();i++){
+            str[indices[i]]=s[i];
+        }
+        return str;
+    }
+};
