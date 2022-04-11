45d29f65da37933f3eed319d922ed24d5233560d
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-04-12 00:09:42 +0530

    Create The kth Factor of n.py

---
+++ LeetCode-Programs/The kth Factor of n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        factors=[]
+        for i in range(1, n+1):
+            if n%i==0:
+                factors.append(i)
+            if len(factors)>k:
+                break
+        if k<=len(factors):
+            return factors[k-1]
+        else:
+            return -1
