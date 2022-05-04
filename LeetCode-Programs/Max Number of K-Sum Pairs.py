7cfd30737c3165d97a28f7dd3a28bb8ea6b07bde
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-05-04 12:34:27 +0530

    Create Max Number of K-Sum Pairs.py

---
+++ LeetCode-Programs/Max Number of K-Sum Pairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        start, end, count= 0, len( nums)-1, 0
+        while start<end:
+            if nums[start] + nums[end]== k:
+                count+=1
+                start+=1
+                end-=1
+            elif nums[start] + nums[end] <k:
+                start+=1
+            else:
+                end-=1
+        return count
