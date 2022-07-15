678d1501b4dbdae8c460c6020302d6d12e1d5574
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-07-15 13:19:10 +0530

    Update Isomorphic Strings.cpp

--- LeetCode-Programs/Isomorphic Strings.cpp
+++ LeetCode-Programs/Isomorphic Strings.cpp
@@ -1,1 +1,18 @@
-//
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,int>m1,m2;
+        for(int i=0; i<s.size(); i++){
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
+        }
+        for(int i=0; i<s.size(); i++){
+            if(m1[s[i]]!=t[i] || m2[t[i]]!=s[i]) 
+                return false;
+        }
+        return true;
+    }
+};
+
+
+//the similar can also be done with the help of character array
