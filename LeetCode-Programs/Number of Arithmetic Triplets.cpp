5c8f9c4f749cd65eead74504728c3a1cc20dd1cd
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-08-10 23:45:06 +0530

    Update Number of Arithmetic Triplets.cpp

--- LeetCode-Programs/Number of Arithmetic Triplets.cpp
+++ LeetCode-Programs/Number of Arithmetic Triplets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {;
-        int ans=0;
+        /*int ans=0;
         for(int i=0; i<nums.size()-2; i++){
             for(int j=i+1; j<nums.size()-1; j++){
                 for(int k=j+1; k<nums.size(); k++){
@@ -10,11 +10,21 @@ public:
                 }
             }
         }
+        return ans;*/
+                                                         
+        int ans=0;
+        unordered_map<int, int> mp;
+        /*for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }*/
+        for(auto i: nums){
+            mp[i]++;
+        }
+        for(auto i: nums){
+            if(i-diff>=0 && mp[i-diff] && mp[i-2*diff])
+                ans++;
+        }
         return ans;
+                                                         
     }
 };
-
-
-
-
-//optimised one
