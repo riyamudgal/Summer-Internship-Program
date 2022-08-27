cd14bc4b936ff151552cc51445921242818c2777
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2022-08-27 23:58:06 +0530

    Update Ransome Note.cpp

--- LeetCode-Programs/Ransome Note.cpp
+++ LeetCode-Programs/Ransome Note.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        sort(ransomNote.begin(),ransomNote.end());
+        /*sort(ransomNote.begin(),ransomNote.end());
         sort(magazine.begin(),magazine.end());
         int i,j,k;
         for(i=0;i<ransomNote.length();i++)
         {
-            c1=count(ransomNote.begin(),ransomNote.end(),ransomNote[i]);
-            c2=count(magazine.begin(),magazine.end(),ransomNote[i]);
-            if(c1>c2) 
+            j=count(ransomNote.begin(),ransomNote.end(),ransomNote[i]);
+            k=count(magazine.begin(),magazine.end(),ransomNote[i]);
+            if(j>k) 
                 return false;
         }
 
-    return true; 
+    return true; */
+        unordered_map<char, int> mp;
+        
+        for(int i=0; i<magazine.length(); i++) {
+            mp[magazine[i]]++;
+        }
+        
+        for(auto i:ransomNote) {
+            if(mp[i]>0) 
+                mp[i]--;
+            else 
+                return false;
+        }
+        
+        return true;
     }
 };
